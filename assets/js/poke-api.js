65f9629b96bc2d607f67e281e02b909005baa28d
LeonardoJr96 <leo70x7000@gmail.com>
2023-08-23 23:01:40 -0300

    update commit: add new image, number in main.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,20 +1,17 @@
 
 const pokeApi = {}
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
     return fetch(url)
         .then((response) => response.json()) // function reduzida (arrow)
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
-
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4')
-]).then((results) => {
-    console.log(results)
-})
