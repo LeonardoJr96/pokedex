ec52ed1487e2a65a93e928ea48eedf58110af614
LeonardoJr96 <leo70x7000@gmail.com>
2023-08-25 22:11:42 -0300

    update commit: New file and update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,16 @@
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {   //função que faz retornar nesse padrão de string
     return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="types">${type}</li>`).join('')}
                 </ol>
 
-            <img src="${pokemon.sprites.other.dream_world.front_default}"
+            <img src="${pokemon.photo}"
                 alt="${pokemon.name}">
             </div>
         </li>
@@ -24,6 +20,6 @@ function convertPokemonToLi(pokemon) {   //função que faz retornar nesse padr�
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join("")
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
     pokemonList.innerHTML = newHtml
 })
